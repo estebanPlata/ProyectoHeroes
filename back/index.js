cf565c49240f947001953959bdc6248e8c71997c
Esteban <eaplata20@gmail.com>
2022-05-07 15:03:35 -0500

    agregamos modelo producto y metodo get donde hacemos peticion de todos los heroes existentes y peticiones por id

--- back/index.js
+++ back/index.js
@@ -2,7 +2,7 @@ const express = require("express");/* creamos variable que contenga express */
 const bodyParser = require('body-parser');/* middleware */
 const mongoose = require("mongoose");
 
-const Product = require("./models/product");
+const Product = require("./models/product.js");
 
 const app= express();
 const port = process.env.port || 4000
@@ -25,3 +25,25 @@ mongoose.connect(uri,/* metodo de coneccion */
 /* promesa */
 .then(()=>console.log('Base de datos conectada'))
 .catch(e=>console.log(e));
+
+app.get('/',(req,res)=>{/* se da un mensaje para saber si se conecto bien a la api */
+    res.send('Node JS registro de superHeroes con mongoDB, creada por: Esteban Plata')
+});
+
+app.get('/superheroes',(req,res)=>{/* accedemos a los heroes*/
+    Product.find({},(err,heroes)=>{
+        if(err) return res.status(500).send({message: `Error al realizar la petición en la base de datos: ${err}`});
+        if(!heroes) return res.status(404).send({message: `Super heroe no encontrado`});
+        res.status(200).send({heroes});
+    })
+});
+
+app.get('/superheroes/:id',(req,res)=>{/* accedemos al id del heroe*/
+    let id = req.params.id
+    Product.findById(id,(err,pro)=>{
+        if(err) return res.status(500).send({message:`Error al realizar la petición en la base de datos: ${err}`});
+        if(!pro) return res.status(404).send({message:`Super heroe no encontrado`});
+        res.status(200).send({id:pro});
+    });
+});
+
