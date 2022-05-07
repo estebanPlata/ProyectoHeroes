d68fae2ecacff3fd00ea3f5a3cb317dc1364a727
Esteban <eaplata20@gmail.com>
2022-05-07 17:00:17 -0500

    se inicio con la refacotizacion del codigo, se creo una carpeta llamada 'controllers' donde se ejecutaran nuestros metodos

--- back/index.js
+++ back/index.js
@@ -2,7 +2,7 @@ const express = require("express");/* creamos variable que contenga express */
 const bodyParser = require('body-parser');/* middleware */
 const mongoose = require("mongoose");
 
-const Product = require("./models/product.js");
+const productCtrl = require("./controllers/product")
 
 const app= express();
 const port = process.env.port || 4000
@@ -26,57 +26,10 @@ mongoose.connect(uri,/* metodo de coneccion */
 .then(()=>console.log('Base de datos conectada'))
 .catch(e=>console.log(e));
 
-app.get('/',(req,res)=>{/* se da un mensaje para saber si se conecto bien a la api */
-    res.send('Node JS registro de superHeroes con mongoDB, creada por: Esteban Plata')
-});
-
-app.get('/superheroes',(req,res)=>{/* accedemos a los heroes*/
-    Product.find({},(err,heroes)=>{
-        if(err) return res.status(500).send({message: `Error al realizar la petición en la base de datos: ${err}`});
-        if(!heroes) return res.status(404).send({message: `Super heroe no encontrado`});
-        res.status(200).send({heroes});
-    })
-});
-
-app.get('/superheroes/:id',(req,res)=>{/* accedemos al id del heroe*/
-    let id = req.params.id
-    Product.findById(id,(err,pro)=>{
-        if(err) return res.status(500).send({message:`Error al realizar la petición en la base de datos: ${err}`});
-        if(!pro) return res.status(404).send({message:`Super heroe no encontrado`});
-        res.status(200).send({id:pro});
-    });
-});
-app.post('/superheroes',(req,res)=>{/* agregamos un heroe*/    
-    let product = new Product();
-    product.name = req.body.name;
-    product.category = req.body.category;
-    product.city = req.body.city;
-    product.status = req.body.status;
-    product.typesOfPower = req.body.typesOfPower;
-    product.car = req.body.car;
-    product.kindOfCar = req.body.kindOfCar;
-
-    product.save((err,pro)=>{
-        if(err) return res.status(500).send({message:`Error al salvar en base de datos: ${err}`})
-        res.status(200).send({product:pro})
-    })
-});
-app.put('/superheroes/:id',(req,res)=>{/* modificamos un heroe segun id*/
-    let id = req.params.id;
-    let upDate = req.body;
-    Product.findByIdAndUpdate(id,upDate,(err,pro)=>{
-        if(err) return res.status(500).send({message:`Error al actualizar el heroe: ${err}`});
-        res.status(200).send({id:pro});
-    })
-});
-app.delete('/superheroes/:id',(req,res)=>{/* borramos un heroe segun id*/
-    let id = req.params.id;/* id contiene el parametro de id  */
-    Product.findById(id,(err,pro)=>{/* funcion para biscar id */
-    if(err) return res.status(500).send({message:`Error al realizar la petición en la base de datos: ${err}`});
-    pro.remove(err=>{
-        if(err) return res.status(500).send({message:`Error al realizar la petición en la base de datos: ${err}`});
-        res.status(200).send({message: 'Super Heroe eliminado'});
-        })
-    })
-});
+app.get('/',productCtrl.getConexion);
+app.get('/superheroes',productCtrl.getProducts);/* accedemos a los heroes*/
+app.get('/superheroes/:id',productCtrl.getProduct);
+app.post('/superheroes',productCtrl.postProduct);
+app.put('/superheroes/:id',productCtrl.putProduct);
+app.delete('/superheroes/:id',productCtrl.deleteProduct);
 
