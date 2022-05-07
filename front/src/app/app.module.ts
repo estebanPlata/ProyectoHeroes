04663b1f997cab370b283250b92479a6ce3ca577
Esteban <eaplata20@gmail.com>
2022-05-06 21:48:03 -0500

    inicializacion de proyecto, tanto front como back tienen instalados sus dependencias necesarias. back: stack nodeJS, express, parser, nodemon, mongoose. front: stack angular, formsModule, reactiveFormsModule, appRoutingModule, httpClientModule

---
+++ front/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DataService } from './shared/services/data.service';
+import {ReactiveFormsModule, FormsModule} from '@angular/forms'
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  providers: [DataService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
