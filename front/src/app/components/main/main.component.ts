b07e94a6a98060ae3e4d2e9064da3155ae93be15
Esteban <eaplata20@gmail.com>
2022-05-09 00:42:15 -0500

    se avanzo gran parte en estructuracion, queda pendiente realizar los metodos put y delete, asociarlos a main.componentent.html, generar directivas constructivas como ngIf para vaidar si tiene coche o no, y modificar diseño de tabla...

--- front/src/app/components/main/main.component.ts
+++ front/src/app/components/main/main.component.ts
@@ -1,4 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/shared/services/data.service';
+/* import {Heroes} from '../../shared/interface/heroes.interface' */
+
+export interface Heroe{
+  name:string;
+  category:string;
+  city:string;
+  status:string;
+  typesOfPower:any;
+  car:boolean;
+  kindOfCar:string;
+
+}
+
 
 @Component({
   selector: 'app-main',
@@ -6,10 +20,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+  dataHeroes:any;
+  heroe: Heroe ={
+    name:'',
+    category:'',
+    city:'',
+    status:'',
+    typesOfPower:'',
+    car:false,
+    kindOfCar:'',
+  };
+  searchValue:any
 
-  constructor() { }
-
+  constructor(private dataService: DataService) { }
   ngOnInit(): void {
+    this.getData();
+  }
+  getData(){
+    this.dataService.getData().subscribe((res)=>{
+      this.dataHeroes = res;
+    })
+  }
+  saveHeroe(){
+    let data={
+      title: this
+    }
   }
 
 }
