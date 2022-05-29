6f6098c53fb191c498ef2ff65402614c101e12d5
Esteban <eaplata20@gmail.com>
2022-05-29 13:40:08 -0500

    save changes

--- front/src/app/shared/pipes/filter.pipe.ts
+++ front/src/app/shared/pipes/filter.pipe.ts
@@ -8,8 +8,8 @@ export class FilterPipe implements PipeTransform {
   transform(items: Array<any>, heroeSearch: string) {
     if (items && items.length) {
       return items.filter(item => {
-        if (heroeSearch && item.name.toString().toLowerCase().indexOf(heroeSearch.toLowerCase()) === -1) {
-          if (heroeSearch && item.city.toString().toLowerCase().indexOf(heroeSearch.toLowerCase()) === -1) {
+        if (heroeSearch && item.name.toString().toLowerCase().indexOf(heroeSearch.toLowerCase()) === -1 ) {
+            if (heroeSearch && item.city.toString().toLowerCase().indexOf(heroeSearch.toLowerCase()) === -1) {
             return false;
           }
           return true;
